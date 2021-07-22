a8297587ffec2ef931448b9e75e5a674a7f69d24
sneha <snehanatekar@whitehatjr.com>
2021-07-23 01:33:33 +0530

    new changes

--- src/SumValidation.java
+++ src/SumValidation.java
@@ -7,17 +7,17 @@ import io.restassured.path.json.JsonPath;
 public class SumValidation {
 
 	@Test
-	public void SumOfCourses () {
-		
-		int sum=0;
+	public void SumOfCourses() {
+
+		int sum = 0;
 		JsonPath js = new JsonPath(payload.CoursePrice());
-		int count  = js.getInt("courses.size()");
-		for (int i=0; i<count; i++) {
-		int pricePerCourse =	js.get("courses["+i+"].price");
-		int copiesPerCourse =	js.get("courses["+i+"].copies");
-		int totalPrice = pricePerCourse * copiesPerCourse;
-		System.out.println(totalPrice);
-		sum = sum + totalPrice;
+		int count = js.getInt("courses.size()");
+		for (int i = 0; i < count; i++) {
+			int pricePerCourse = js.get("courses[" + i + "].price");
+			int copiesPerCourse = js.get("courses[" + i + "].copies");
+			int totalPrice = pricePerCourse * copiesPerCourse;
+			System.out.println(totalPrice);
+			sum = sum + totalPrice;
 		}
 
 		System.out.println(sum);
--- src/files/payload.java
+++ src/files/payload.java
@@ -49,5 +49,17 @@ public class payload {
 				+ "  ]\r\n"
 				+ "}";
 	}
+	
+	public static String addBook(String isbn, String aisle)
+	{
+		String payload = "{\r\n"
+		+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
+		+ "\"isbn\":\""+isbn+"\",\r\n"
+		+ "\"aisle\":\""+aisle+"\",\r\n"
+		+ "\"author\":\"John foe\"\r\n"
+		+ "}\r\n"
+		+ "";
+		return payload;
+	}
 
 }
